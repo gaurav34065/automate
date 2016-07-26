9899e3e36d757bc2c36fcdc6949210c786d7c2cf
gaurav34065 <gaurav_sharma34065@yahoo.com>
2016-07-26 12:15:29 +0530

    Update default.rb

--- cookbooks/load-balancer/recipes/default.rb
+++ cookbooks/load-balancer/recipes/default.rb
@@ -2,6 +2,10 @@ package 'haproxy' do
   action :install
 end
 
+package 'socat' do
+  action :install
+end
+
 #edit haproxy.cfg
 template "#{node['haproxy']['config_path']}/haproxy.cfg" do
   source 'haproxy.cfg.erb'
@@ -9,7 +13,8 @@ template "#{node['haproxy']['config_path']}/haproxy.cfg" do
   			   :algorithm => node['haproxy']['algorithm'],
   			   :stats_enable => node['haproxy']['is_stats_enabled'],
   			   :stats_uri => node['haproxy']['stats_uri'],
-  			   :backend_servers => node['haproxy']['backend_servers']
+  			   :backend_servers => node['haproxy']['backend_servers'],
+           :socket => node['haproxy']['socket']
   			})
   action :create
 end
@@ -27,3 +32,14 @@ service 'haproxy' do
   action :restart
 end
 
+# run the backend processes/daemons
+script 'run_haproxy_scripts' do
+  interpreter 'bash'
+  user 'root'
+  cwd '/'
+  code <<-EOH
+  cd /vagrant/scripts
+  ./lb_haproxy_stats_processor.sh >/vagrant/scripts/logs/lb_haproxy_stats_processor.out 2>/vagrant/scripts/logs/lb_haproxy_stats_processor.err &
+  ./lb_haproxy_file_editor.sh >/vagrant/scripts/logs/lb_haproxy_file_editor.out 2>/vagrant/scripts/logs/lb_haproxy_file_editor.err &
+  EOH
+end
