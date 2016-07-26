88158c21c056197d1faac51e0a2f6c36946881de
gaurav34065 <gaurav_sharma34065@yahoo.com>
2016-07-26 12:28:53 +0530

    Delete test.sh

--- scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./lb_haproxy_stats_processor.sh >/vagrant/scripts/logs/lb_haproxy_stats_processor.out 2>/vagrant/scripts/logs/lb_haproxy_stats_processor.err &
-./lb_haproxy_file_editor.sh >/vagrant/scripts/logs/lb_haproxy_file_editor.out 2>/vagrant/scripts/logs/lb_haproxy_file_editor.err &
